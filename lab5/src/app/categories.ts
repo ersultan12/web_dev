2acd122f0992aa55c5d28749d91c081767aade2d
yersultan <erserstur@gmail.com>
2023-03-02 01:24:15 +0600

    here I added the files category-list, category-item, categories.ts, card.service to category-list just added html. In categories.ts, I export Category, gave data types and added data, then opened an array where each product will be stored. In the app.module.ts linked all the components together

---
+++ lab5/src/app/categories.ts
@@ -0,0 +1,24 @@
+export interface Category {
+    categoryId: number,
+    name: string;
+}
+
+
+export const categories: Category[] = [
+    {
+        categoryId: 1,
+        name: 'phone',
+    },
+    {
+        categoryId: 2,
+        name: 'laptop',
+    },
+    {
+        categoryId: 3,
+        name: 'TV',
+    },
+    {
+        categoryId: 4,
+        name: 'Sport',
+    },
+]
