f6e9730344826e550028a5125aabe876c13e3f68
yersultan <erserstur@gmail.com>
2023-03-03 10:21:06 +0600

    setted product-alerts

--- lab5/src/app/product-alerts/product-alerts.component.ts
+++ lab5/src/app/product-alerts/product-alerts.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Product } from '../products';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+
 @Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  selector: 'app-product-alerst',
+  templateUrl: './product-alerst.component.html',
+  styleUrls: ['./product-alerst.component.css']
 })
-export class ProductAlertsComponent {
-  @Input() product: Product | undefined;
-  @Output() notify = new EventEmitter();
+export class ProductAlerstComponent implements OnInit {
+
+  product?: Product;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    const productCategory = String(routeParams.get('categoryName'));
+    //console.log(productIdFromRoute)
+    //console.log(productCategory)
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute && product.categoryName === productCategory);
+  }
 }
